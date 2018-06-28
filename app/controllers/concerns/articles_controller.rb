b2ac9e7b6a7e102ee0ce5d268d757f2cca911979
Rails Udemy Course <mughal.swaqas@gmail.com>
2018-06-28 05:57:20 -0400

    Create rails application and setup home and about page

---
+++ app/controllers/concerns/articles_controller.rb
@@ -0,0 +1,61 @@
+class ArticlesController < ApplicationController
+  def new
+    @article =Article.new
+    
+  end
+  
+  def index
+  @articles=Article.all    
+    
+  end
+   
+  def edit
+    @article=Article.find(params[:id])
+    
+  end
+  
+  def create
+   @article=Article.new(article_params)
+    if @article.save
+    flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else 
+      render 'new'
+    end
+  end
+  
+  
+  
+   def update
+    @article=Article.find(params[:id])
+     if @article.update(article_params)
+       flash[:notice] ="Article Update was successfull!"
+      redirect_to article_path(@article)
+     else
+       render 'edit' 
+     end
+  end
+  
+  def show
+    @article=Article.find(params[:id])
+    
+  end
+  
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    flash[:notice]= "Article was Successfully Deleted"
+    redirect_to articles_path
+    
+    
+  end
+ 
+  
+  private
+  def article_params
+    params.require(:article).permit(:title,:description)
+      
+  end
+    
+  
+end
